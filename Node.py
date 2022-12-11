c0da98b3c463938256fff4a58abc69ead25da8df
raashidkhan1 <raashid.khan1@gmail.com>
2022-12-11 22:11:19 +0100

    add voting logic

--- Node.py
+++ Node.py
@@ -33,16 +33,16 @@ class Node :
         print("Starting XMLRPC Server on node", self.node_name)
         self.server = Server(self.node_name, self.port)
         self.server.register_function(self.message_received, "message_received")
+        self.server.register_function(self.send_vote, "send_vote")
         self.server.start()
         print("XMLRPCServer started")
 #        sock.close()
 
         ##testing with node107 as leader
-        if self.node_name == "node107":
-            self.state="leader"
+        # if self.node_name == "node107":
+        #     self.state="leader"
 
         self.start_loop_thread()
-        # self.node_self_loop()
 
 
     ## basic functions
@@ -72,42 +72,60 @@ class Node :
             t.join()
 
     # in case no hearbeat and timer ran out, self vote and request vote RPC
-    def request_vote(self):
-        pass
+    def request_vote(self, node, port):
+        with ServerProxy ('http://'+node+':'+ str(port)) as rpc_call:
+
+            print("requesting vote from:", node)
+
+            response = rpc_call.send_vote(self.term)
+
+            if response:
+                print(f"Node, {node} sent vote")
+                self.vote_count += 1
+            else :
+                print("Did not receive vote")
+                return
 
     # timer logic for
     def timer(self):
-        return random.randrange(100, 120)
+        return random.randrange(1, 5)
 
     def append_entries(self, node, port):
         with ServerProxy ('http://'+node+':'+ str(port)) as rpc_call:
 
-            logs=[]
-            leader= self.node_name
-
             print("Sending heartbeat to:",node)
 
-            response = rpc_call.message_received(leader,logs)
+            response = rpc_call.message_received(self.term)
 
             if response:
-                print("Node,{node} recieved hearbeats")
+                print(f"Node, {node} received hearbeats")
+                if(self.term < response):
+                    self.term = response
+                    self.state = self.states[1]
+                    
             else :
                 print("Did not receive")
                 return
 
-    def message_received(self,leader,logs):
-        print("I'm node:",self.node_name)
-        print("Message:",self.heartbeat_received)
+    def send_vote(self, term):
+        if self.state != self.states[2] and self.term < term:
+            return True
+        else:
+            print("Iam a candidate or your current term is low, no voting")
+            return False
+
+    def message_received(self, term):
+        print("I'm node:", self.node_name)
+        print("heartbeat received:", self.heartbeat_received)
         self.heartbeat_received = True
-        print("The leader is:",leader)
-        return True
+        if self.term < term: 
+            self.term = term
+            return term
+        elif self.term > term:
+            return self.term
+        else:
+            return term
 
-
-
-    # election vote count check
-    def check_election_winner(self):
-        ## count votes, send win/lose response
-        return False
 
     ### functions for each type of node
 
@@ -117,21 +135,37 @@ class Node :
 
     def candidate(self):
         print('Im a candidate, bro!')
+        self.vote_count +=1
+        self.term +=1
+        all_request_threads = []
 
-#            self.term +=1
-#            self.vote_count +=1
-#            ## TODO: call election aka request votes rpc
-#            print(f'{self.node_name} wants to contest the leader election')
-#            if self.vote_count == 1:
-#                self.state = self.states[1]
+        for node in self.cluster_nodes:
+            thread = threading.Thread(target=self.request_vote, args=(node['name'], node['port']))
+            thread.start()
+            all_request_threads.append(thread)
+
+        for t in all_request_threads:
+            t.join()
+        
+        ## check vote count
+        if(self.vote_count >= self.cluster_nodes.size()/2):
+            print(f"{self.node_name} says: Iam the leader now bitch")
+            self.state = self.states[0]
+            self.vote_count = 0
+        else:
+            print(f"{self.node_name} says: failed to collect votes, becoming follower again")
+            self.state = self.states[1]
+            self.vote_count = 0
 
     def follower(self):
         print('Im a follower, bro!')
-        print("Follower message :",self.heartbeat_received)
+        print("Follower message :", self.heartbeat_received)
         self.heartbeat_received = False
-        #time.sleep(self.election_timeout)
-#        if not self.heartbeat_received:
-#            self.state=self.states[2]
+        time.sleep(self.election_timeout)
+        ## check leader term and sync own term
+
+        if not self.heartbeat_received:
+           self.state=self.states[2]
 
 
     def node_self_loop(self):
@@ -197,7 +231,7 @@ if __name__ == '__main__':
     except Exception as e:
         print("Exception", e)
 
-            # exit handler *terminates threads and kills server
+    # exit handler *terminates threads and kills server
     atexit.register(my_node.handle_exit)
     signal.signal(signal.SIGTERM, my_node.handle_exit)
     signal.signal(signal.SIGINT, my_node.handle_exit)
